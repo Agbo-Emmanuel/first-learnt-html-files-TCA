f81f735de04a616a89378116ae8a98a1f527dcc8
Agbo-Emmanuel <agboe4102@gmail.com>
2024-08-07 01:18:42 +0100

    hhh

---
+++ ass o1/index.js
@@ -0,0 +1,76 @@
+// let myinfo=(name,age)=>{
+    
+// console.log("my name is "+age +" i am "+name +"years" )
+
+// }
+
+// myinfo( 1,"emmanuel")
+
+
+
+
+// let  calculator=(a,b)=>{
+
+// console.log(a+b)
+// }
+
+
+// calculator(2,2)
+
+// let calculator=(a,b)=>{
+//     a=10
+//     b=20
+//     console.log(b-a)
+// }
+
+// calculator(100,50)
+
+
+// let calculator=(a,b)=>{
+    
+
+//     a+b
+//     console.log(a+b)
+// }
+
+// console.log(calculator(100,50))
+
+
+
+// for(let gbolo = 0; gbolo<10; gbolo++){
+//     console.log("my loop is running" +gbolo)
+// }
+
+
+// for(let gbolo = 0; gbolo<=10; gbolo+=2){
+//     console.log("my loop is running" +gbolo)
+// }
+
+// let arr = ["ffranchesca", "mchinex","fcynthia","mdivine","ffavour","misaac","frose"]
+
+
+
+// for (let index = 0; index < arr.length; index++) {
+    
+//     let firstChar = arr[index][0]
+//     if (firstChar =="f"){
+//         console.log(arr[index])
+//     }
+    
+    
+// }
+
+// let arr = ["ffranchesca", "mchinex","fcynthia","mdivine","ffavour","misaac","frose"]
+
+
+
+// for (let index = 0; index < arr.length; index++) {
+    
+//     let firstChar = arr[index][0]
+//     if (firstChar =="f"){
+//         console.log(arr[index].slice(1) +" you are a female")
+//     }
+    
+    
+// }
+
